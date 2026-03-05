b2342b850e4c6fe68c772e1e4e210c6e226543a5
Adrian-DG <adrian96guilamo@gmail.com>
2026-03-05 12:15:59 -0400

    Add pagination support to FastAPI application

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from database import engine
 from routers import auth, todos
 import tables
+from fastapi_pagination import add_pagination
 
 app = FastAPI()
+add_pagination(app)
 
 tables.Base.metadata.create_all(bind=engine)
 
